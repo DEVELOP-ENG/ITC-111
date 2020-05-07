178fe861633118a0c1c2863e15236944c0728f2a
DEVELOP-ENG <60213834+DEVELOP-ENG@users.noreply.github.com>
2020-05-07 12:57:50 -0700

    yes
    yup

---
+++ classEx/variables_Practice.js
@@ -0,0 +1,18 @@
+var a =5; //this defines a variable 
+
+var a;
+
+a = 5; //here we're creating the variable, and defining it later
+
+console.log(a); //console.log creates a string to print the text
+
+var a,b,c;  //here we're using variables to create functions
+a = 10;
+b = 45;
+c = b-a
+
+var a = 5 
+function apple() {
+    var a = 50
+    return a;
+}
