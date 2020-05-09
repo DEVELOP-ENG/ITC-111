a61fc1116f882ccc0a6478ffe0783cca9a7d7cba
DEVELOP-ENG <60213834+DEVELOP-ENG@users.noreply.github.com>
2020-05-08 22:26:27 -0700

    promp

---
+++ js/index.js
@@ -0,0 +1,17 @@
+//console.log('Hello World')
+
+var firstName = 'Noel'
+var message = 'hello world'
+var isTrue = true;
+var someVar; 
+console.log(firstName)
+
+function printToConsole(message){
+ 
+    var what = message;
+    return what;
+}
+
+
+
+console.log(printToConsole(message));
---
+++ js/practice.js
@@ -0,0 +1,10 @@
+
+var number = prompt("Please enter a number between 1-16:");
+//var count = prompt(number);
+var count =[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, A, B, C, D, E, F];
+
+document.write(count);
+
+//if (person != null) {
+  //document.getElementById("demo").innerHTML =
+  //"Hello " + person + "! How are you today?";
