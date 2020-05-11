464857fb07345f4947b73aba55be0f3d90f6e780
DEVELOP-ENG <60213834+DEVELOP-ENG@users.noreply.github.com>
2020-05-11 12:35:56 -0700

    1
    2

---
+++ js/more_practice.js
@@ -0,0 +1,10 @@
+//The function prompt returns a string. If you want to verify this you can do alert(typeof count)
+
+//You need to convert this string to an integer, and there is a well known function for this operation.
+//Use this integer variable to then index into your array
+var msg = "Please enter number betweem 1-16:"; 
+var count = prompt(msg);
+var num1 = parseInt(document.getElementById(count).value);
+var HexArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D" , "E", "F"];
+
+document.getElementById(num1).value = HexArray[count - 1];
--- js/practice.js
+++ js/practice.js
@@ -1,10 +1,17 @@
+function myFunction() {
+    var HexArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"]
+    var count = parseInt(document.getElementById("count").value);
+        if (count > 16 || count < 1)
+    {
+        alert("Range must be between 1 and 16")
+        return
+    }
+    document.getElementById("result").value = HexArray[count - 1];
+    }
 
-var number = prompt("Please enter a number between 1-16:");
-//var count = prompt(number);
-var count =[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, A, B, C, D, E, F];
+var number = ("Please enter number between 1-16:");
+var count = prompt(number);
+var convert = parseInt( count - 1 )
+var hex =[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, A, B, C, D, E, F];
 
-document.write(count);
-
-//if (person != null) {
-  //document.getElementById("demo").innerHTML =
-  //"Hello " + person + "! How are you today?";
+console.log(hex.LastIndexOf( convert )
