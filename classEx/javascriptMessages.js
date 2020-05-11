0cb4149896137c8ed776e1e0cef3524ae00398b4
DEVELOP-ENG <60213834+DEVELOP-ENG@users.noreply.github.com>
2020-05-11 13:18:14 -0700

    Update javascriptMessages.js

--- classEx/javascriptMessages.js
+++ classEx/javascriptMessages.js
@@ -1,19 +1,38 @@
-var str = 'this is a string.';
+//var str = 'this is a string.';
 
 //length method = returns the size of the string
 
-var strlen = str.length;
+//var strlen = str.length;
 
 //go to characher at specified string index in array
-var stridx = str[8];
+//var stridx = str[8];
 
 //index.of method = get position of character in string
-var stridx = str.indexOf(a);
+//var stridx = str.indexOf(a);
 
 //slice method = get asubstring or specified portion of a string.center
-var strslice = str.slice(5,7);
+//var strslice = str.slice(5,7);
 
-var strupper = str.toLocaleUpperCase();
-var strlower = str.toLocaleLowerCase();
+// touppercase() and tolowercase() - change the case of a string character 
+//var strupper = str.toLocaleUpperCase();
+//var strlower = str.toLocaleLowerCase();
 
-console.log(strupper + ' ' + strlower);
+//replace() method - replace character of a string
+//var strRpl = str.replace('a', 'our');
+
+
+//number methods 
+//var  num = ['75'];
+
+
+
+//arrays 
+
+var fruits = ['apples', 'oranges', 'grapes', 'lemons', 'bannanas'];
+var type = [];
+
+//array methods
+
+//length method - gets the number of elements in an array
+
+console.log(fruits.length); 
