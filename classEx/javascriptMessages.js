d05621dec20240123db445da314c250a6944c562
DEVELOP-ENG <60213834+DEVELOP-ENG@users.noreply.github.com>
2020-05-11 12:59:28 -0700

    we good
    yes

---
+++ classEx/javascriptMessages.js
@@ -0,0 +1,19 @@
+var str = 'this is a string.';
+
+//length method = returns the size of the string
+
+var strlen = str.length;
+
+//go to characher at specified string index in array
+var stridx = str[8];
+
+//index.of method = get position of character in string
+var stridx = str.indexOf(a);
+
+//slice method = get asubstring or specified portion of a string.center
+var strslice = str.slice(5,7);
+
+var strupper = str.toLocaleUpperCase();
+var strlower = str.toLocaleLowerCase();
+
+console.log(strupper + ' ' + strlower);
--- js/more_practice.js
+++ js/more_practice.js
@@ -7,4 +7,4 @@ var count = prompt(msg);
 var num1 = parseInt(document.getElementById(count).value);
 var HexArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D" , "E", "F"];
 
-document.getElementById(num1).value = HexArray[count - 1];
+document.getElementById("result").value = HexArray[count - 1];
