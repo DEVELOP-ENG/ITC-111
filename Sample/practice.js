5b39a93f2ba308b7cca0a0641db796bb8ded8ae7
DEVELOP-ENG <60213834+DEVELOP-ENG@users.noreply.github.com>
2020-06-01 13:26:19 -0700

    new code and calc
    added new code for calc

---
+++ Sample/practice.js
@@ -0,0 +1,7 @@
+
+
+function greeting("firstName", "lastName") {
+    var firstName = document.getElementById(firstName)
+    var lastName = document.getElementById(lastName)
+    var greeting = `Hello ${firstName} ${lastName}, how are you doing?`
+    document.getElementById("greeting") =} 
