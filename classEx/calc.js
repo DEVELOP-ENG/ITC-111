977e2d6b4d0a4378e3b6822ca75089eafc2e8ce6
DEVELOP-ENG <60213834+DEVELOP-ENG@users.noreply.github.com>
2020-06-04 13:39:34 -0700

    Update calc.js

--- classEx/calc.js
+++ classEx/calc.js
@@ -2,24 +2,19 @@ var val1, val2;
 function loadDisplay(num) {
     document.getElementById('display2').value += num.value.toString();
 }
-
-function delFromRight(){
-    document.getElementById('display2').value = document.getElementById('display2').value.slice(0, -1);
+function delFromRight() {
+    document.getElementById('display2').value = document.getElementById('display2').value.slice(0,-1);
 }
+function clearDisplay() {
+    document.getElementById('display2').value = ""
 
-function clearDisplay(){
-    document.getElementById('display2').value + " " + tag.value;
+}
+function setOperator(trg) {
+    val1 = document.getElementById('display2').value + " " + trg.value;
     document.getElementById('display1').innerHTML = val1;
     document.getElementById('display2').value = "";
 }
-
-function setOp(tag) {
-    val1 = document.getElementById('display2').value + " " + tag.value;
-    document.getElementById('display1').innerHTML = val1;
-    document.getElementById('display2').value = "";
-}
-
-function doMath(){
+function doMath() {
     val2 = document.getElementById('display2').value;
     var result;
     switch(val1.slice(-1)){
@@ -28,17 +23,32 @@ function doMath(){
             break;
         case '-':
             result = parseInt(val1) - parseInt(val2);
+            break;
         case '*':
             result = parseInt(val1) * parseInt(val2);
             break;
         case '/':
             result = parseInt(val1) / parseInt(val2);
             break;
-    }
+        case 'sqrt':
+            // do square root math
+            break;
+        case 'pow':
+            // do eponent math
+            break;
 
+
+    }
     document.getElementById('display1').innerHTML = "";
     document.getElementById('display2').value = result;
-    val1= null;
+    val1 = null;
     val2 = null;
     result = null;
 }
+//function specialCase(){
+//    if(sqrt){
+//        // do square root math
+//    }else{
+//       // do exponent math
+//    }
+// }
