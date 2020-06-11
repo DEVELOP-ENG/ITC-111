9af4c321dfc1d093e336e051e9b7344f36e09b09
Noel Eng <60213834+DEVELOP-ENG@users.noreply.github.com>
2020-06-11 10:40:01 -0700

    1
    1

--- toDoList/To-Do-List-master/js/app.js
+++ toDoList/To-Do-List-master/js/app.js
@@ -1,1 +1,26 @@
 // CODE EXPLAINED channel
+
+//select element 
+const clear = document.querySelector(".clear");
+const dataElement = document.getElementById("date");
+const list = document.getElementById ("list");
+const input = document.getElementById("input");
+
+//classes names 
+const CHECK = "fa-check-circle";
+const UNCHECK = "fa-circle-thin";
+const LINE_THROUGH = "lineThrough";
+
+//Show todays date
+const options = {weeday : "long", month:"short", day:"numeric"};
+const today = new Date();
+dataElement.innerHTML = today.toLocaleDateString("en-US", options)
+function addToDo(toDo){
+    const item = '
+                <li class = "item"
+                <i class="fa fa-circle-thin co" job = "complete" id="0"></i>
+                <p class="text">{toDo}</p>
+                <i class="fa fa-trash-o de" job = "delete" id="0"></i>
+                </li> 
+                ';
+}
